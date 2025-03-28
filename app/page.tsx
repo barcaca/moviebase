862f467cba838e7a73339971a61f5ce0643df0d7
barcaca <73846225+barcaca@users.noreply.github.com>
2025-03-28 17:52:46 -0300

    feat: integrar ShowcaseCarousel e HeroSection à página home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import HeroSection from '@/components/hero-section'
+import ShowcaseCarousel from '@/components/showcase-carousel'
+
 export default function Home() {
-  return <div>Ola mundo</div>
+  return (
+    <div className="mx-auto ">
+      <HeroSection
+        title="Bem-vindo ao"
+        subtitle="Moviebase"
+        description="Descubra filmes e séries de diversos gêneros, de clássicos atemporais a
+          lançamentos imperdíveis. Uma experiência completa e prática para quem
+          ama cinema e séries."
+      />
+
+      <ShowcaseCarousel title="Destaques" aspectRatio="poster" />
+      <ShowcaseCarousel title="Novidades" aspectRatio="square" />
+      <ShowcaseCarousel title="Popular" aspectRatio="poster" />
+      <ShowcaseCarousel title="Recomendados" aspectRatio="square" />
+
+      <footer>{/* Footer */}</footer>
+    </div>
+  )
 }
