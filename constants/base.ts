20fa5b0ed5398236b1684e3374fe10fe082fa6f4
barcaca <73846225+barcaca@users.noreply.github.com>
2025-03-31 17:08:13 -0300

    chore: atualizar constantes de API

--- constants/base.ts
+++ constants/base.ts
@@ -1,5 +1,7 @@
 /* Base Url of the TMDB API */
 
+import { env } from '@/env'
+
 export const BASE_URL = {
   REST: 'https://api.themoviedb.org/3',
 } as const
@@ -13,3 +15,16 @@ export const LANGUAGES = {
   BR: '?language=pt-BR',
   EN: '?language=en-US',
 } as const
+
+export const DEFAULT_DISCOVER_PARAMS = {
+  includeAdult: false,
+  includeVideo: false,
+  language: 'pt-BR',
+  page: 1,
+  sortBy: 'popularity.desc',
+} as const
+
+export const COMMON_HEADERS = {
+  accept: 'application/json',
+  Authorization: `Bearer ${env.TMDBAPI_TOKEN}`,
+}
