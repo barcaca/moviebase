630868e64e9150f91a7e8fed085e736d5833759a
barcaca <73846225+barcaca@users.noreply.github.com>
2025-03-31 17:39:08 -0300

    feat: integrar na pagina novo service

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import HeroSection from '@/components/hero-section'
 import ShowcaseCarousel from '@/components/showcase-carousel'
 import { getPopularMonth } from '@/data/populars'
+import { getTheatres } from '@/data/theatres'
 import { getTrending } from '@/data/trending'
 
 export default async function Home() {
   const trendings = await getTrending()
   const popular = await getPopularMonth()
+  const theatres = await getTheatres()
+
   return (
     <div className="mx-auto ">
       <HeroSection
@@ -15,18 +18,24 @@ export default async function Home() {
           lançamentos imperdíveis. Uma experiência completa e prática para quem
           ama cinema e séries."
       />
-
+      {/* On Theatres */}
+      <ShowcaseCarousel
+        items={theatres}
+        title="No Cinema"
+        aspectRatio="poster"
+      />
+      {/* Trendings */}
       <ShowcaseCarousel
         items={trendings}
         title="Destaques"
         aspectRatio="poster"
       />
+      {/* Popular */}
       <ShowcaseCarousel
         items={popular}
         title="Popular do mês"
         aspectRatio="poster"
       />
-      <ShowcaseCarousel title="Novidades" aspectRatio="square" />
       <ShowcaseCarousel title="Recomendados" aspectRatio="square" />
     </div>
   )
