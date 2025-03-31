754f8ffaeb8fd609375b92dbb11716a3641f5400
barcaca <73846225+barcaca@users.noreply.github.com>
2025-03-31 17:09:19 -0300

    feat: integrar getPopular na página principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/components/hero-section'
 import ShowcaseCarousel from '@/components/showcase-carousel'
-import { getTrending } from '@/data/data'
+import { getPopularMonth } from '@/data/populars'
+import { getTrending } from '@/data/trending'
 
 export default async function Home() {
   const trendings = await getTrending()
+  const popular = await getPopularMonth()
   return (
     <div className="mx-auto ">
       <HeroSection
@@ -19,8 +21,12 @@ export default async function Home() {
         title="Destaques"
         aspectRatio="poster"
       />
+      <ShowcaseCarousel
+        items={popular}
+        title="Popular do mês"
+        aspectRatio="poster"
+      />
       <ShowcaseCarousel title="Novidades" aspectRatio="square" />
-      <ShowcaseCarousel title="Popular" aspectRatio="poster" />
       <ShowcaseCarousel title="Recomendados" aspectRatio="square" />
     </div>
   )
