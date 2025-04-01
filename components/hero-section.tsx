83c07e91d6e8d2f90620eb7e2b16185243d85dd3
barcaca <73846225+barcaca@users.noreply.github.com>
2025-04-01 16:48:00 -0300

    refactor: tornar subtitle opcional no HeroSection

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,6 +1,6 @@
 interface HeroSectionProps {
   title: string
-  subtitle: string
+  subtitle?: string
   description: string
 }
 
@@ -8,7 +8,7 @@ const HeroSection = ({ title, subtitle, description }: HeroSectionProps) => {
   return (
     <section className="mx-auto flex h-[13rem] max-w-2xl flex-col justify-center space-y-3 pb-6 text-center lg:h-[30rem]">
       <h1 className="mb-4 flex flex-col font-bold text-2xl text-foreground sm:text-5xl lg:text-7xl ">
-        {title} <span className="text-primary">{subtitle}</span>
+        {title} {subtitle && <span className="text-primary">{subtitle}</span>}
       </h1>
       <p className="text-muted-foreground text-sm sm:text-lg lg:text-2xl">
         {description}
